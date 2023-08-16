c298c056bb0578697aaca404ea2eb129711aab8a
Luis Jove <47277829+starman45@users.noreply.github.com>
2023-08-16 11:05:46 -0500

    Update odoo-backup.sh

--- odoo-backup.sh
+++ odoo-backup.sh
@@ -1,79 +1,88 @@
 #!/bin/bash
 
-# Función para comprobar variables de entorno
-check_env_var() {
-    if [ -z "${!1}" ]; then
-        echo "ERROR: Instalation error, $1 is not defined"
-        exit 1
-    fi
-}
 
-# Comprobar variables de entorno necesarias
-required_env_vars=("DB_ENV_POSTGRES_USER" "HOME" "DB_PORT_5432_TCP_ADDR" "ODOO_DATA_DIR" "DB_ENV_POSTGRES_PASSWORD")
-for var in "${required_env_vars[@]}"; do
-    check_env_var "$var"
-done
-
-# Obtener la fecha actual
-NOW=$(date '+%Y-%m-%d_%H%M%S')
-
-# Función para realizar copia de seguridad de la base de datos
-backup_database() {
-    database="$1"
-    logfile="${database}_${NOW}-backup.log"
-    db_password=$DB_ENV_POSTGRES_PASSWORD
-
-    echo "BACKUP: DATABASE = $database, TIME = $NOW" > "$logfile"
-
-    if ! PGPASSWORD="$db_password" /usr/bin/psql -h "$DB_PORT_5432_TCP_ADDR" -U "$DB_ENV_POSTGRES_USER" -l -F'|' -A "template1" | grep "|$DB_ENV_POSTGRES_USER|" | cut -d'|' -f1 | egrep -q "^$database$"; then
-        echo "ERROR: Database '$database' not found for user '$DB_ENV_POSTGRES_USER'"
-        exit 2
-    fi
-
-    echo -n "Backup database: $database ... "
-    PGPASSWORD="$db_password" /usr/bin/pg_dump -Fc -v -U "$DB_ENV_POSTGRES_USER" --host "$DB_PORT_5432_TCP_ADDR" -f "${database}_${NOW}.dump" "$database" >> "$logfile" 2>&1
-    error=$?
-    if [ $error -eq 0 ]; then
-        echo "OK"
-    else
-        echo "ERROR: $error"
-    fi
-}
-
-# Función para realizar copia de seguridad del directorio
-backup_filestore() {
-    database="$1"
-    logfile="${database}_${NOW}-backup.log"
-
-    if [ ! -d "$ODOO_DATA_DIR/filestore/$database" ]; then
-        echo "ERROR: Filestore '$ODOO_DATA_DIR/filestore/$database' not found"
-        exit 3
-    fi
-
-    echo -n "Backup filestore: $ODOO_DATA_DIR/filestore/$database ... "
-    /bin/tar -C "$ODOO_DATA_DIR/filestore" -czf "$HOME/backup/${database}_filestore_${NOW}.tar.gz" "$database" >> "$logfile" 2>&1
-    error=$?
-    if [ $error -eq 0 ]; then
-        echo "OK"
-    else
-        echo "ERROR: $error"
-    fi
-}
-
-# Realizar copia de seguridad de la base de datos
-backup_database "$1"
-
-# Realizar copia de seguridad del directorio
-backup_filestore "$1"
-
-# Comprimir todos los archivos de respaldo en un archivo tar.gz
-echo -n "Compressing backup files: ... "
-/bin/tar -czf "${1}_${NOW}.tar.gz" "${1}_${NOW}.dump" "${HOME}/backup/${1}_filestore_${NOW}.tar.gz" >> "$logfile" 2>&1
-error=$?
-if [ $error -eq 0 ]; then
-    echo "OK"
-else
-    echo "ERROR: $error"
+### Enviroment Variables needed to run the odoo-backup script ###
+### that most of them are found in the .env file of the project ###
+if [ -z "$DB_ENV_POSTGRES_USER" ]; then
+    echo "ERROR: Instalation error, DB_ENV_POSTGRES_USER is not defined"
+    exit 1
 fi
-echo "${1}_${NOW}.tar.gz"
+if [ -z "$HOME" ]; then
+    echo "ERROR: Instalation error, HOME is not defined"
+    exit 1
+fi
+if [ -z "$DB_PORT_5432_TCP_ADDR" ]; then
+    echo "ERROR: Instalation error, DB_PORT_5432_TCP_ADDR is not defined"
+    exit 1
+fi
+if [ -z "$ODOO_DATA_DIR" ]; then
+    echo "ERROR: Instalation error, ODOO_DATA_DIR is not defined"
+    exit 1
+fi
 
+if [ -z "$DB_ENV_POSTGRES_PASSWORD" ]; then
+    echo "ERROR: Instalation error, DB_ENV_POSTGRES_PASSWORD is not defined"
+    exit 1
+fi
+
+
+### Setting date value for the construct name of the backup files ###
+NOW=`date '+%Y-%m-%d_%H%M%S'`
+
+
+### Database name parameter that must be given to the script to generate the backup files of the database given ###
+database="$1"
+if [ -z "$database" ]; then
+    echo "ERROR: No database"
+    echo "Usage: $0 <database>"
+    exit 1
+fi
+
+
+### Setting logfile value for the backup.log file generated ###
+logfile="${database}_${NOW}-backup.log"
+
+
+### Placing us on the backup directory and sending data to the logfile ###
+mkdir -p $HOME/backup
+cd $HOME/backup
+echo "BACKUP: DATABASE = $database, TIME = $NOW" > $logfile
+
+
+### Setting the postgress password value ###
+db_password=$DB_ENV_POSTGRES_PASSWORD
+echo
+
+
+### Validating the database name given exists for the given postgres user ###
+if ! PGPASSWORD="$db_password" /usr/bin/psql -h $DB_PORT_5432_TCP_ADDR -U "$DB_ENV_POSTGRES_USER" -l -F'|' -A "template1" | grep "|$DB_ENV_POSTGRES_USER|" | cut -d'|' -f1 | egrep -q "^$database\$"; then
+    echo "ERROR: Database '$database' not found for user '$DB_ENV_POSTGRES_USER'"
+    exit 2
+fi
+
+
+### Validating the path for the database name given exists ###
+if [ ! -d "$ODOO_DATA_DIR/filestore/$database" ]; then
+    echo "ERROR: Filestore '$ODOO_DATA_DIR/filestore/$database' not found"
+    exit 3
+fi
+
+
+### Generating the .dump backup file for the given database name ###
+echo -n "Backup database: $database ... "
+PGPASSWORD="$db_password" /usr/bin/pg_dump -Fc -v -U "$DB_ENV_POSTGRES_USER" --host $DB_PORT_5432_TCP_ADDR -f "${database}_${NOW}.dump" "$database" >> $logfile 2>&1
+error=$?; if [ $error -eq 0 ]; then echo "OK"; else echo "ERROR: $error"; fi
+
+
+### Generating the filestore backup file for the given database name ###
+echo -n "Backup filestore: $ODOO_DATA_DIR/filestore/$database ... "
+/bin/tar -C "$ODOO_DATA_DIR/filestore" -czf "$HOME/backup/${database}_filestore_${NOW}.tar.gz" $database >> $logfile 2>&1
+error=$?
+if [ $error -eq 0 ]; then echo "OK"; else echo "ERROR: $error"; fi
+
+### Compressing all the generated backup files into one .tar.gz file ###
+echo -n "Compressing backup files: ... "
+/bin/tar -czf "${database}_${NOW}.tar.gz" *
+error=$?
+if [ $error -eq 0 ]; then echo "OK"; else echo "ERROR: $error"; fi
+echo ${database}_${NOW}.tar.gz
